4a1aeced3f07c66beb1d0eaeb5e96f53b8189be7
U. T. Thushani <136692213+tharikathushani@users.noreply.github.com>
2024-08-31 23:02:49 +0530

    Add files via upload
    create crud operations

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "ap1/v1")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/userdelete")
+    public String deteteUser(@RequestBody UserDTO userDTO){
+        return userService.deteteUser(userDTO);
+    }
+
+
+
+}
